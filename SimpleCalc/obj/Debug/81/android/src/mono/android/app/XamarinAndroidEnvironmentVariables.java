6233817431835cefae83503096dc58efe2d30517
Frank Odoom <f.odoom@outlook.com>
2019-04-22 09:42:42 +0000

    Added demo Image drawable

--- SimpleCalc/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ SimpleCalc/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "b4e9cd7c-5342-4b7a-b3ea-d61e3c1ead9f",
+		"XAMARIN_BUILD_ID", "80bc6f86-be13-45b7-82fa-2543486bce45",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
